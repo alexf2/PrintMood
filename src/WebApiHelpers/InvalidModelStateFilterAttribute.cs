734e236be654e6a08ac3bc6dfe3706c3e72c9829
Alexey Fedorov <Aleksey-Fedorov@mailfrom.ru>
2016-09-16 02:29:55 +0300

    Added InvalidModelStateFilterAttribute

---
+++ src/WebApiHelpers/InvalidModelStateFilterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApiHelpers
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class InvalidModelStateFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ModelState.IsValid)
+                return;
+            context.Result = new BadRequestObjectResult(context.ModelState);                
+        }
+    }
+}
