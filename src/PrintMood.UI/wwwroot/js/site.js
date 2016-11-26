a5be7fe57beeab90b9bbc03e3e3ec743a63ba102
Alexey Fedorov <Aleksey-Fedorov@mailfrom.ru>
2016-11-26 17:58:32 +0300

    A small fix

--- src/PrintMood.UI/wwwroot/js/site.js
+++ src/PrintMood.UI/wwwroot/js/site.js
@@ -112,8 +112,8 @@
 
     /****Smooth Scrolling***/
     $('a[href*=\\#]:not([href=\\#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-            || location.hostname == this.hostname) {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+            || location.hostname === this.hostname) {
 
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
